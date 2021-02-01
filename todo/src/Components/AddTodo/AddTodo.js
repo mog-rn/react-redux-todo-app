4d53f9d6b5952a2065a135cd88977cf381b0896c
Mogakamo <mogaka.amo254@gmail.com>
2021-02-01 20:33:44 +0300

    Commit

--- todo/src/Components/AddTodo/AddTodo.js
+++ todo/src/Components/AddTodo/AddTodo.js
@@ -6,6 +6,24 @@ import { addTodo } from '../../redux/actions'
 
 class AddTodo extends React.Component {
     //...component implementation
+
+    handleAddTodo = () => {
+        //dispatches actions to add todo
+        this.props.addTodo(this.state.input)
+
+        //sets state back to empty string
+        this.setState({ input: ''})
+    }
+    render() {
+        return (
+            <div>
+                <input
+                    onChange={e => this.updateInput(e.target.value)}
+                    value={this.state.input}
+                />    
+            </div>
+        )
+    }
 }
 
 export default connect(
