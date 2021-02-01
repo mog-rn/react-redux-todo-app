3ccc2265d1643b0c0f0d0eaf72352a8bed29b627
Mogakamo <mogaka.amo254@gmail.com>
2021-02-01 20:50:51 +0300

    Commit

---
+++ todo/src/Components/TodoList/TodoList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+const TodoList = //...UI component implementation 
+
+const mapStateToProps = state => {
+    const { byIds, allIds } = state.todos || {};
+    const todos = 
+        allIds && allIds.length
+            ? allIds.map(id => (byIds ? { ...byIds[id], id} : null))
+            : null;
+            return { todos }
+}
+
+export default connect(mapStateToProps)(TodoList);
